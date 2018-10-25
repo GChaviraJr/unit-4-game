2f5991b9a0a18d2380e646840c1b7fc7d4db11c7
Lupe <lupec_se@yahoo.com>
2018-10-24 17:57:20 -0700

    Began pseudo code for JS as well as started actual code for JQuery

---
+++ assets/javascript/game.js
@@ -0,0 +1,30 @@
+// Global Var remember to debug/ console log
+$(document).ready(function() {
+    
+    
+    var redCrystalElement = document.getElementById("redCrystal");
+    var blueCrystalElement = document.getElementById("blueCrystal");
+    var yellowCrystal = "";
+    var greenCrystal = "";
+});
+
+
+// Crytals random number generator 1-12 - CONSOLE LOG
+
+
+
+
+// Random Number shown at Start of game 19-120 - CONSOLE LOG
+
+
+
+// click funcitons from user entering numbers
+
+
+
+// Restart game win player wins/ loses - CONSOLE LOG
+
+
+
+
+// Wins Losses shown - CONSOLE LOG
